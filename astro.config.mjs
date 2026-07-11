2834ef37db026cd58b1bcfe4b9b10efff5b48a02
inestrivino <inestriv@ucm.es>
2026-07-11 17:29:00 +0200

    Base estética

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://inestrivino.github.io',
+
   server: {
     port: 3000
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
