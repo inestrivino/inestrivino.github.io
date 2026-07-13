45ab85006457b8c5e2b2297dfe674e42675eefef
inestrivino <inestriv@ucm.es>
2026-07-14 00:03:44 +0200

    About me page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,6 +17,8 @@ export const ui = {
     'tooltip.menu': 'Menú',
     'tooltip.cv': 'VER CV',
 
+    'art.alt': 'Arte Ascii decorativo',
+
     'home.title': 'Inés Triviño | Inicio',
     'home.description': "Página portfolio de Inés Triviño",
     'home.proyectos_recientes': 'Proyectos recientes',
@@ -41,6 +43,17 @@ export const ui = {
     'contenido.tabla': 'Tabla de contenidos',
     'contenido.noSecciones': 'No hay secciones',
     'contenido.noContenido': 'Aún no hay nada aquí.',
+
+    'about.title': 'Inés Triviño - Sobre mí',
+    'about.description': 'Página para informarse más sobre mi y mis habilidades informáticas',
+    'about.titulo': 'Sobre mí',
+    'about.technologies': 'Tecnologías, herramientas e idiomas',
+    'about.parrafo1':
+      '¡Hola! Soy Inés, ingeniera de software con un perfil versátil que complementa mi conocimiento técnico con mi formación actual en Letras Digitales(UCM).Mi objetivo es fusionar ambas disciplinas para desarrollar software robusto y de calidad aplicado al ámbito cultural y la investigación.',
+    'about.parrafo2':
+      'En cada diseño e implementación priorizo las arquitecturas limpias, la mantenibilidad y la calidad del código. Ante cualquier reto técnico, investigo a fondo y exploro diferentes metodologías para dar con la solución óptima, asegurándome siempre de plasmar mi línea de pensamiento en una documentación clara y detallada.',
+    'about.parrafo3':
+      'Cuando me alejo del ordenador, me encontrarás haciendo deporte, leyendo o disfrutando de algún videojuego.',
   },
   en: {
     'nav.inicio': 'home',
@@ -52,6 +65,8 @@ export const ui = {
     'tooltip.tema': 'Theme',
     'tooltip.menu': 'Menu',
     'tooltip.cv': 'READ CV',
+
+    'art.alt': 'Decorative Ascii art',
 
     'home.title': 'Inés Triviño | Home',
     'home.description': "Portfolio website for Inés Triviño",
@@ -75,6 +90,16 @@ export const ui = {
     'contenido.tabla': 'Contents table',
     'contenido.noSecciones': 'There are no sections',
     'contenido.noContenido': 'There is nothing here yet.',
+
+    'about.title': 'Inés Triviño - About me',
+    'about.description': 'Page to get more info on me and my tech abilities',
+    'about.titulo': 'About me',
+    'about.technologies': 'Tech, tools and languages',
+    'about.parrafo1': "Hi! I'm Inés, a software engineer with a versatile profile that complements my technical background with my current studies in Digital Humanities (UCM). My goal is to merge both fields to develop robust, high-quality software applied to the cultural and research sectors.",
+
+    'about.parrafo2': "In every design and implementation, I prioritize clean architecture, maintainability, and code quality. Faced with any technical challenge, I dive deep into research and explore different methodologies to find the optimal solution, always making sure to reflect my line of thought in clear and detailed documentation.",
+
+    'about.parrafo3': "When I step away from the computer, you'll find me working out, reading, or playing video games."
   },
 } as const;
 
